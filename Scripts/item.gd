8c806de81b250bbdabd023dabf5443490045909b
lazlo barragan <lazlo.barragan@gmail.com>
2019-08-03 18:22:25 +0200

    items follow mouse

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -1,9 +1,30 @@
 extends Area2D
 
+onready var TweenNode=get_node("Tween")
+var follow=false
+var area_over
+var current_area
+
 func _ready():
-	pass
+	current_area=get_parent();
 
+func _process(delta):
+	if (follow):
+		global_position=get_global_mouse_position()
 
-func _on_Item_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton && event.pressed):
-		print("y")
+func _input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton):
+		if(event.pressed):
+			follow=true
+		else:
+			if(area_over and !area_over.full):
+				current_area=area_over
+			follow=false
+			global_position=current_area.global_position
+
+func _area_entered(area):
+	if("Slot" in area.name):
+		area_over=area
+func _area_exited(area):
+	if("Slot" in area.name):	
+		area_over=null
